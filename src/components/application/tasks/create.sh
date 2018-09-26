5df7d718a1bf30d9015e75e94976e0f041c75c99
Oliver Nural <onural@and.digital>
2018-09-26 21:10:20 +0100

    fixed some of the issues

--- src/components/application/tasks/create.sh
+++ src/components/application/tasks/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "CREATE - Attempting to create Task: portfolio-task"
-aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-task --template-body file://./template.json --parameters file://./parameters.json
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-task --template-body file://template.json --parameters file://parameters.json
 
 echo "WAITING - waiting on create Task: portfolio-task"
 aws cloudformation wait stack-create-complete --stack-name portfolio-task
--- src/components/network/subnet/create.sh
+++ src/components/network/subnet/create.sh
@@ -1,5 +1,5 @@
-echo "CREATE - Attempting to create ECR: portfolio-repository"
-aws cloudformation create-stack --region eu-west-2 --stack-name portfolio-repository --template-body file://./vpc-template.json --parameters file://./parameters.json
+echo "CREATE - Attempting to create Subnet: portfolio-subnet"
+aws cloudformation create-stack --region eu-west-2 --stack-name portfolio-subnet --template-body file://vpc-template.json --parameters file://parameters.json
 
-echo "WAITING - waiting on create stack"
-aws cloudformation wait stack-create-complete --stack-name portfolio-repository
+echo "WAITING - waiting on create Subnet: portfolio-subnet"
+aws cloudformation wait stack-create-complete --stack-name portfolio-subnet
