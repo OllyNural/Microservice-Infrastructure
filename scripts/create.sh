19b91f86c48f15c7ae9d197dd0153f8ec195c365
Oliver Nural <onural@and.digital>
2018-09-26 22:35:39 +0100

    fixed create script subnet creation

--- scripts/create.sh
+++ scripts/create.sh
@@ -13,7 +13,7 @@ cd ../../../
 
 cd components/network/subnet
 echo "CREATE - Attempting to create Subnet: portfolio-subnet"
-aws cloudformation create-stack --region eu-west-2 --stack-name portfolio-subnet --template-body file://template.json --parameters file://parameters.json
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-subnet --template-body file://template.json --parameters file://parameters.json
 
 echo "WAITING - waiting on create Subnet: portfolio-subnet"
 aws cloudformation wait stack-create-complete --region eu-west-1 --stack-name portfolio-subnet
