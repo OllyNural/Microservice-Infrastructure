4b28b4ef8c0d577920dbb88ede7ef148f0718859
Oliver Nural <onural@and.digital>
2018-09-26 18:53:04 +0100

    debugging circle

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sh ../src/components/application/ecr/create.sh
+sh src/components/application/ecr/create.sh
 
-# sh ../src/components/application/cluster/create.sh
+# sh src/components/application/cluster/create.sh
 
-# sh ../src/components/application/tasks/create.sh
+# sh src/components/application/tasks/create.sh
 
-# sh ../src/components/application/ec2/create.sh
+# sh src/components/application/ec2/create.sh
 
-# sh ../src/components/application/services/create.sh
+# sh src/components/application/services/create.sh
