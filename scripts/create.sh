b1afa99a24d246d7db260dbc908a3b7a5e636624
Oliver Nural <onural@and.digital>
2018-09-26 20:58:42 +0100

    Added all components!

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,10 +2,24 @@
 
 cd src
 
-cd components/application/ecr
+# Network
+cd components/network/vpc
 sh create.sh
 cd ../../../
 
+cd components/network/subnet
+sh create.sh
+cd ../../../
+
+# Application
+
+cd components/application/ec2
+sh create.sh
+cd ../../../
+
+cd components/application/ecr
+sh create.sh
+cd ../../../
 
 cd components/application/cluster
 sh create.sh
@@ -18,10 +32,6 @@ cd ../../../
 cd components/application/services
 sh create.sh
 cd ../../../
-
-# cd components/application/ec2
-# sh create.sh
-# cd ../../../
 
 
 # sh src/components/application/tasks/create.sh
