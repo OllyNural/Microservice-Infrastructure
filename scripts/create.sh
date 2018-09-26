034e254a36210529c8d99f897b3fdf682552b61a
Oliver Nural <onural@and.digital>
2018-09-26 21:24:10 +0100

    Added bugfixing of the circleci confgi

--- scripts/create.sh
+++ scripts/create.sh
@@ -4,38 +4,51 @@ cd src
 
 # Network
 cd components/network/vpc
-sh create.sh
+echo "CREATE - Attempting to create VPC: portfolio-vpc"
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-vpc --template-body file://template.json --parameters file://parameters.json
+
+echo "WAITING - waiting on create VPC: portfolio-vpc"
+aws cloudformation wait stack-create-complete --stack-name portfolio-vpc
 cd ../../../
 
 cd components/network/subnet
-sh create.sh
+echo "CREATE - Attempting to create Subnet: portfolio-subnet"
+aws cloudformation create-stack --region eu-west-2 --stack-name portfolio-subnet --template-body file://template.json --parameters file://parameters.json
+
+echo "WAITING - waiting on create Subnet: portfolio-subnet"
+aws cloudformation wait stack-create-complete --stack-name portfolio-subnet
 cd ../../../
 
 # Application
 
 cd components/application/tasks
-sh create.sh
+echo "CREATE - Attempting to create Task: portfolio-task"
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-task --template-body file://template.json --parameters file://parameters.json
+
+echo "WAITING - waiting on create Task: portfolio-task"
+aws cloudformation wait stack-create-complete --stack-name portfolio-task
 cd ../../../
 
 cd components/application/ec2
-sh create.sh
-cd ../../../
+echo "CREATE - Attempting to create EC2: portfolio-ec2"
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-ec2 --template-body file://template.json --parameters file://parameters.json
 
-cd components/application/ecr
-sh create.sh
+echo "WAITING - waiting on create EC2: portfolio-ec2"
+aws cloudformation wait stack-create-complete --stack-name portfolio-ec2
 cd ../../../
 
 cd components/application/cluster
-sh create.sh
+echo "CREATE - Attempting to create Cluster: portfolio-cluster"
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-cluster --template-body file://template.json --parameters file://parameters.json
+
+echo "WAITING - waiting on create Cluster: portfolio-cluster"
+aws cloudformation wait stack-create-complete --stack-name portfolio-cluster
 cd ../../../
 
 cd components/application/services
-sh create.sh
+echo "CREATE - Attempting to create Services: portfolio-service"
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-service --template-body file://template.json --parameters file://parameters.json
+
+echo "WAITING - waiting on create Services: portfolio-service"
+aws cloudformation wait stack-create-complete --stack-name portfolio-service
 cd ../../../
-
-
-# sh src/components/application/tasks/create.sh
-
-# sh src/components/application/ec2/create.sh
-
-# sh src/components/application/services/create.sh
