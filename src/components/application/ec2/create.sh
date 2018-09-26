862e32e9379b35aa4f8f8170b2ca64f88424ab6d
Oliver Nural <onural@and.digital>
2018-09-26 18:57:19 +0100

    fixing bash debug

--- src/components/application/ec2/create.sh
+++ src/components/application/ec2/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "CREATE - Attempting to create EC2: portfolio-ec2"
-aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-ec2 --template-body file://./template.json --parameters file://./parameters.json
+aws cloudformation create-stack --region eu-west-1 --stack-name portfolio-ec2 --template-body file://template.json --parameters file://parameters.json
 
 echo "WAITING - waiting on create EC2: portfolio-ec2"
 aws cloudformation wait stack-create-complete --stack-name portfolio-ec2
